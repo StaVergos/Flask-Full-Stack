445713a31e346445829b8d863441cd8fd89a9f77
Stavros Vergos <stavrada@gmail.com>
2022-10-29 23:00:20 +0300

    Feature: Added templates

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('home.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
