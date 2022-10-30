e63c1d7ef95c989626559600a7f03f7cbd507564
Stavros Vergos <stavrada@gmail.com>
2022-10-30 18:55:15 +0200

    Feature: Created queries from UserModel

--- query_engine.py
+++ query_engine.py
@@ -1,4 +1,6 @@
 from pymongo import MongoClient
+from models.user_model import UserModel
+from typing import List, Dict, Any
 
 
 client = MongoClient('mongodb://localhost:27017')
@@ -7,12 +9,24 @@ db = client['flask-full-stack']
 users_auth = db['users']
 
 
-def insert_user(user):
-    try:
-        user_id = users_auth.db.insert_one(user).inserted_id
-        return user_id
-    except:
-        return {
-            'success': False,
-            'message': 'Failed inserting user to database'
-        }
+def db_create_user(user: MongoClient) -> bool:
+    users_auth.insert_one(user.__dict__)
+
+
+def db_update_book(user_id: str, book: Dict[str, Any]) -> bool:
+    res = users_auth.update_one({"id": user_id}, {"$set": user})
+    return res.modified_count > 0
+
+
+def db_list_books() -> List[UserModel]:
+    return [UserModel.from_dict(r) for r in users_auth.find()]
+
+
+def db_retrieve_user(user_id: str) -> UserModel:
+    _book = users_auth.find_one({"id": user_id})
+    return UserModel.from_dict(_book) if _book else None
+
+
+def db_delete_book(user_id: str) -> bool:
+    res = users_auth.delete_one({"id": user_id})
+    return res.deleted_count > 0
