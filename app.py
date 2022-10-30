fa52c741674c6b51d95dc53978a26040a361d519
Stavros Vergos <stavrada@gmail.com>
2022-10-30 19:05:39 +0200

    Feature: Added create_user endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, url_for, request, jsonify
+from models.user_model import UserModel
+from query_engine import db_create_user
 
 
 app = Flask(__name__)
@@ -18,3 +19,16 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
+@app.route('/create_user')
+def create_user():
+    payload = request.get_json()
+    if "id" in payload:
+        payload.pop("id")
+        status = UserModel.validate(payload, partial=("id",))
+        if status:
+            return jsonify(status), 400
+        user = UserModel.from_dict(payload)
+        db_create_user(user)
+        return jsonify(data=user.to_dict()), 201
