61fb5911d38d55fa2382e69e9fc525ccd911cc38
Stavros Vergos <stavrada@gmail.com>
2022-10-30 14:46:12 +0200

    Feature: Deleted redundant user_model.py

--- models/user_model.py
+++
@@ -1,8 +0,0 @@
-from app import db
-from flask_login import UserMixin
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(32), nullable=False)
